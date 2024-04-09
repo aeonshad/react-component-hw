9945be2b89713ac748cfac592bf964356cb6ace8
aeonshad <shadaeon@gmail.com>
2024-04-09 13:57:45 +0200

    Creation of the design for the modal and the test page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import Modal from './components/Modal';
+import { ReactComponent as CheckIcon } from './assets/verified.svg';
+import './styles/main.css';
+import './styles/reset.css';
 
 function App() {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,13 +12,26 @@ function App() {
     };
 
     return (
-        <div>
-            <button onClick={toggleModal}>Open Modal</button>
+        <main className="main-container">
+            <button className="modal-btn-open" onClick={toggleModal}>
+                Open Modal
+            </button>
             <Modal isOpen={isOpen} onClose={toggleModal}>
-                <h1>Modal Content</h1>
-                <p>The content has been added</p>
+                <div className="modal-content">
+                    <CheckIcon className="modal-img" />
+                    <h1 className="modal-title">Congratulations!</h1>
+                    <p className="modal-text">You are ready to use modal-component-hw</p>
+                    <div className="modal-link-container">
+                        <a href="#" className="modal-link secondary" onClick={toggleModal}>
+                            Return
+                        </a>
+                        <a href="/" className="modal-link primary">
+                            Refresh
+                        </a>
+                    </div>
+                </div>
             </Modal>
-        </div>
+        </main>
     );
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './modal.css';
+import { ReactComponent as CloseIcon } from './close-outline.svg';
 
 function Modal({ isOpen, onClose, children }) {
     return isOpen ? (
         <div className="modal-overlay">
-            <div className="modal">
-                <button onClick={onClose}>Close</button>
+            <div className="modal-container">
+                <button className="modal-btn-close" onClick={onClose}>
+                    <CloseIcon className="modal-btn-close-icon" />
+                </button>
                 {children}
             </div>
         </div>
